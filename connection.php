92e877ea980bd7354e39df3c031ef557bc6f1ab3
Sandeep <sk9958814616@gmail.com>
2020-01-10 18:43:35 +0530

    update in connection

--- connection.php
+++ connection.php
@@ -1,7 +1,8 @@
 <?php
 //Connect to the database
 //this file is to maintain the connection data
-$link = mysqli_connect("localhost","swingoff_note","OORb3K6K5t","swingoff_onlinenotes");
+//enter here the database connection details
+$link = mysqli_connect("host","database username","database password","database name");
 if(mysqli_connect_error()){
     die("ERROR: Unable to connect:".mysqli_connect_error());
     echo "<script>window.alert('Hi!')</script>";
